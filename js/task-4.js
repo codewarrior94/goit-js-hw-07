34a2eadf959f61461df3745d6265a8bcde76d25a
Dmytro Troitskiy <dmitriysabbath@gmail.com>
2024-04-27 21:53:46 +0300

    Resolve task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+// Get form reference
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+  const userData = {
+    email: "",
+    password: "",
+  };
+
+  if (login === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  userData.email = login;
+  userData.password = password;
+
+  console.log(userData);
+
+  form.reset();
+}
