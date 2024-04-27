760b553e8c81c1cef3ba7e6799d826aca25d360d
Dmytro Troitskiy <dmitriysabbath@gmail.com>
2024-04-27 23:31:53 +0300

    Resolve task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,18 @@
+// Define references for elements
+const getWidget = document.querySelector(".widget");
+const getSpan = document.querySelector(".color");
+const getButton = document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+getButton.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  const generatedColor = getRandomHexColor();
+  getWidget.style.backgroundColor = generatedColor;
+  getSpan.textContent = generatedColor;
 }
